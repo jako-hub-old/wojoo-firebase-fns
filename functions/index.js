665a1a1deead6370806cb91672f6a19a98fb17d9
Alejandro <jakop.box@gmail.com>
2019-05-15 14:09:08 -0500

    Wojo message function

---
+++ functions/index.js
@@ -0,0 +1,32 @@
+const express       = require('express');
+const cors          = require('cors');
+const functions     = require('firebase-functions');
+const admin         = require('firebase-admin');
+const bodyParser    = require('body-parser');
+
+const app = express();
+app.use(cors({origin : true}));
+app.use(bodyParser())
+app.post('/to', (req, res) => {
+    const token = req.body.token;
+    const body = req.body.message;
+    const title = req.body.title;
+    const data = req.body.data||{};
+    data.title = title;
+    data.body = body;
+    const payload = {
+        notification : {
+            title,body
+        },
+        data : data,
+    };
+    const sended = admin.messaging().sendToDevice(token, payload)
+        .then(reponse => {
+            return res.send({ message : "sended!", token, payload : payload })
+        })
+        .catch(response => {
+            return res.send({ message : "error!", data : response, token, payload })
+        });
+});
+
+exports.sendPushNotification = functions.https.onRequest(app);
